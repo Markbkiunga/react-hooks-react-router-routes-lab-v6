2e5833ac0809bbddfbac91a61f4102a950f664ab
Mark Brian <markbkiunga2017@gmail.com>
2024-08-12 22:05:45 +0300

    Yet to complete lab

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,12 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function MovieCard({title}) {
-  
+function MovieCard({ title, time, id }) {
   return (
-    <article>
-        <h2>{title}</h2>
-        {/* What should go here? */}
+    <article key={id}>
+      <h1>{title}</h1>
+      <p>{time}</p>
     </article>
   );
-};
+}
 
 export default MovieCard;
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import NavBar from '../components/NavBar';
+import { useParams } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
 
 function Movie() {
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
       </main>
     </>
   );
-};
+}
 
 export default Movie;
