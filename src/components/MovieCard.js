248fd30e424058890c2518df03931e8817b89807
Mark Brian <markbkiunga2017@gmail.com>
2024-08-17 13:40:30 +0300

    Renders a list for every movie

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 function MovieCard({ title, id }) {
   return (
-    <>
+    <li>
       <h2>{title}</h2>
       <button className="movie-button">
         <Link to={`/movie/${id}`}>View Info</Link>
       </button>
-    </>
+    </li>
   );
 }
 
